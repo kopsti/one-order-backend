1fe477225646980eadcd7461ac9ce25cca3aef9c
Timos Kopsidas <kopsidas.t@gmail.com>
2019-10-07 21:39:04 +0300

    Some basic authentication to avoid indexing

--- src/one_order/settings/prod.py
+++ src/one_order/settings/prod.py
@@ -8,3 +8,9 @@ ALLOWED_HOSTS = os.environ.get('HOST')
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600)
 }
+
+MIDDLEWARE += [
+    'baipw.middleware.BasicAuthIPWhitelistMiddleware'
+]
+BASIC_AUTH_LOGIN = os.environ.get('USER_ROLE')
+BASIC_AUTH_PASSWORD = os.environ.get('USER_PASS')
